6820f828f528a0bf99746d84febd69cd18855835
SC <70444245+SC136@users.noreply.github.com>
2024-02-15 22:40:41 +0530

    update

--- commands/music/play.js
+++ commands/music/play.js
@@ -8,7 +8,7 @@ module.exports = {
 	async execute(interaction) {
 		const player = useMainPlayer();
 		const channel = interaction.member.voice.channel;
-		if (!channel) return interaction.reply({ embeds: [new EmbedBuilder().setDescription('```you are not connected to a voice channel')] }); // make sure we have a voice channel
+		if (!channel) return interaction.reply({ embeds: [new EmbedBuilder().setDescription('```you are not connected to a voice channel```')] }); // make sure we have a voice channel
 		const query = interaction.options.getString('query', true); // we need input/query to play
 
 		// let's defer the interaction as things can take time to process
