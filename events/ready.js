4511627471b18d4f524afbe994bfc4deb08e93d8
SC <70444245+SC136@users.noreply.github.com>
2024-02-15 22:35:00 +0530

    update

--- events/ready.js
+++ events/ready.js
@@ -5,9 +5,7 @@ module.exports = {
 	once: true,
 	execute(client) {
 		console.log(`${client.user.tag} started in ${client.guilds.cache.size} servers with ${client.users.cache.size} members`);
-		client.user.setActivity(`Simplicity | </help:1207727681005948938> | Simply Simple Music Bot!`, {
-			type: "LISTENING",
-		});
+		client.user.setPresence({ activities: [{ name: 'Simplicity | </help:1207727681005948938> | Simply Simple Music Bot!', type: 2 }] });
 		client.channels.cache
 			.get("850294318290829372")
 			.send({ embeds: [new EmbedBuilder().setDescription(`\`\`\`bot started in ${client.guilds.cache.size} servers with ${client.users.cache.size} members\`\`\``)] });
